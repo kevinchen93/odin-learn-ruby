a5d3368b65349f3e4cef8bfae87878c33b0ac5a9
Kevin Chen <xkevinchen@gmail.com>
2018-02-08 19:17:59 -0500

    initial commit

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,28 @@
+#write your code here
+
+def add(a, b)
+	return a + b
+end
+
+def subtract(a, b)
+	return a - b
+end
+
+def sum(array)
+	total = 0
+		array.each do |x| 
+		total+=x
+		end
+	return total 
+end
+
+def multiply(a, b)
+	return a * b
+end
+
+def power(a, b)
+	return a ** b
+end
+
+def factorial(n)
+	while 
